66fa3a959a7a0930191b49eaa8d001c0c9e8e704
Dmitrii Lih <omts1dm@gmail.com>
2023-02-12 12:37:31 +0500

    задача

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат (умножает число само на себя).
 
-Console.Write ("Введите два числа:");
+Console.Write ("Введите число:");
 int number =  Convert.ToInt32(Console.ReadLine());
 int square = 0;
 square = number * number;
